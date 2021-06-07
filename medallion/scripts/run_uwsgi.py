eb47036a767607fd33077d4a57e39285187a4468
Hugo CHASTEL <hugo.chastel@orange.com>
2021-06-07 17:41:30 +0200

    MRTI-3500 # Fix flask config for uwsgi

--- medallion/scripts/run_uwsgi.py
+++ medallion/scripts/run_uwsgi.py
@@ -6,24 +6,21 @@ from medallion.scripts.run import _get_argparser
 
 LOG_FILE = 'app.log'
 
-if __name__ == "__main__":
-    logging.basicConfig(filename=LOG_FILE,
-                        filemode='a',
-                        format='%(name)s %(levelname)s %(message)s',
-                        level=logging.INFO)
-    log = logging.getLogger("medallion")
+log = logging.getLogger("medallion")
+fh = logging.FileHandler('app.log')
+fh.setFormatter(logging.Formatter("[%(name)s] [%(levelname)-8s] %(message)s"))
+log.addHandler(fh)
 
-    log.info("Setting up medaillon in uwsgi mode")
-    medallion_parser = _get_argparser()
-    medallion_args = medallion_parser.parse_args()
-    log.setLevel(medallion_args.log_level)
+log.info("Setting up medaillon in uwsgi mode")
+medallion_parser = _get_argparser()
+medallion_args = medallion_parser.parse_args()
+log.setLevel(medallion_args.log_level)
 
-    with open(medallion_args.CONFIG_PATH, "r") as f:
-        configuration = json.load(f)
+with open(medallion_args.CONFIG_PATH, "r") as f:
+    configuration = json.load(f)
 
-    set_config(application_instance, "users", configuration)
-    set_config(application_instance, "taxii", configuration)
-    set_config(application_instance, "backend", configuration)
-    register_blueprints(application_instance)
-    log.debug(f"host: {medallion_args.host}")
-    log.info("Medaillon set up")
+set_config(application_instance, "users", configuration)
+set_config(application_instance, "taxii", configuration)
+set_config(application_instance, "backend", configuration)
+register_blueprints(application_instance)
+log.critical("Medaillon config set up")
