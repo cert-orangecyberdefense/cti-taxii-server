ec224dd934f3092f97a05d6f63ee62e7dfbf9944
Hugo-C <Hugo-C@users.noreply.github.com>
2021-06-08 15:13:10 +0200

    Feature/mrti 3500 Configure flask app to be used by uwsgi (#1)
    * MRTI-3500 # Adapt docker for uwsgi
    
    Add log file
    Codecov require an API key, removed it until we have one

---
+++ medallion/scripts/run_uwsgi.py
@@ -0,0 +1,26 @@
+import json
+import logging
+
+from medallion import application_instance, register_blueprints, set_config
+from medallion.scripts.run import _get_argparser
+
+LOG_FILE = 'app.log'
+
+log = logging.getLogger("medallion")
+fh = logging.FileHandler('app.log')
+fh.setFormatter(logging.Formatter("[%(name)s] [%(levelname)-8s] %(message)s"))
+log.addHandler(fh)
+
+log.info("Setting up medaillon in uwsgi mode")
+medallion_parser = _get_argparser()
+medallion_args = medallion_parser.parse_args()
+log.setLevel(medallion_args.log_level)
+
+with open(medallion_args.CONFIG_PATH, "r") as f:
+    configuration = json.load(f)
+
+set_config(application_instance, "users", configuration)
+set_config(application_instance, "taxii", configuration)
+set_config(application_instance, "backend", configuration)
+register_blueprints(application_instance)
+log.critical("Medaillon config set up")
